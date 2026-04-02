ba6888b4eac88599f6069a51624ca9284e52999b
Arnold Angote <arnoldangote9@gmail.com>
2026-04-02 05:14:16 +0300

    Created a test controller for busines rules

---
+++ src/main/java/com/arnold/autolibrary/controller/SchoolController.java
@@ -0,0 +1,30 @@
+package com.arnold.autolibrary.controller;
+
+import com.arnold.autolibrary.model.SchoolClass;
+import com.arnold.autolibrary.services.SchoolClassService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/classes")
+public class SchoolController {
+    @Autowired
+    private SchoolClassService schoolClassService;
+
+    @PostMapping
+    public ResponseEntity<?>createClass(@RequestBody SchoolClass schoolClass){
+        try{
+            SchoolClass created = schoolClassService.addClass(schoolClass);
+            return ResponseEntity.status(HttpStatus.CREATED).body(created);
+        }catch(RuntimeException e ){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+}
